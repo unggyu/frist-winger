1eb4b959eb8e0b924bb9a3cc734e6ba5827413eb
Choi-UngGyu <cdr982116@gmail.com>
2021-04-29 22:34:07 +0900

    플레이어 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 moveVector = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
+
+    }
+
+    public void ProcessInput(Vector3 moveDirection)
+    {
+
+    }
+}
