d40c8d0194664e82eb15ee116f4d6bc77c4603fc
Choi-UngGyu <cdr982116@gmail.com>
2021-05-02 17:07:47 +0900

    패널 매니저 구현

---
+++ Assets/Scripts/BasePanel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class BasePanel : MonoBehaviour
+{
+    public virtual void InitializePanel()
+    {
+        PanelManager.RegistPanel(GetType(), this);
+    }
+
+    public virtual void DestroyPanel()
+    {
+        PanelManager.UnregistPanel(GetType());
+    }
+
+    public virtual void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        InitializePanel();
+    }
+
+    private void OnDestroy()
+    {
+        DestroyPanel();
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Close"))
+        {
+            Close();
+        }
+    }
+}
---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    private static readonly Dictionary<Type, BasePanel> panels = new Dictionary<Type, BasePanel>();
+
+    public static bool RegistPanel(Type panelClassType, BasePanel basePanel)
+    {
+        if (panels.ContainsKey(panelClassType))
+        {
+            Debug.LogError("RegistPanel Error! Already exists Type! panelClassType = " + panelClassType.ToString());
+            return false;
+        }
+
+        Debug.Log("RegisterPanel is called! Type = " + panelClassType.ToString() + ", basePanel = " + basePanel.name);
+
+        panels.Add(panelClassType, basePanel);
+        return true;
+    }
+
+    public static bool UnregistPanel(Type panelClassType)
+    {
+        if (!panels.ContainsKey(panelClassType))
+        {
+            Debug.LogError("UnregistPanel Error! Can't find type! panelClassType = " + panelClassType.ToString());
+            return false;
+        }
+
+        panels.Remove(panelClassType);
+        return true;
+    }
+
+    public static BasePanel GetPanel(Type panelClassType)
+    {
+        if (!panels.ContainsKey(panelClassType))
+        {
+            Debug.LogError("GetPanel Error! Can't find type! panelClassType = " + panelClassType.ToString());
+            return null;
+        }
+
+        return panels[panelClassType];
+    }
+}
